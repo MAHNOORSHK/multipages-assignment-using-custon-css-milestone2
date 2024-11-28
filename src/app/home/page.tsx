657065074b5ecff6882ad70498c09d02ffafa8f9
MAHNOORSHK <[mahnoorshaikh1066@gmail.com]>
2024-11-28 17:54:41 +0500

    first commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -126,7 +126,7 @@ export default function Home(){
             <div className="main">
                 <div className="textBlog">
                 <h1>Our Blogs</h1>
-                <p>Our blog is a treasure trove of informative and engaging articles written by our team of nutritionists, dietitians, and wellness experts. Here's what you can expect from our blog.</p>
+                <p>Our blog is a treasure trove of informative and engaging articles written by our team of nutritionists, dietitians, and wellness experts. Here is what you can expect from our blog.</p>
                 </div>
 
                 <div className="cardBlog">
